49a622df9f586f2fe6667d6cf0f08da6f38f0e8f
Iyadh Gdouda <iadh.gdouda@istic.ucar.tn>
2025-11-01 13:39:37 +0100

    3eme le focntionnemt du jeux parfait

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/main.dart';
+import 'package:eco_warrior_tunisia1/main.dart';
 
 void main() {
   testWidgets('App starts with splash screen', (WidgetTester tester) async {
-    // Lance l'application
     await tester.pumpWidget(const EcoWarriorTunisia());
-
-    // Vérifie que le splash screen s'affiche
     expect(find.text('ECO WARRIOR'), findsOneWidget);
     expect(find.text('TUNISIA'), findsOneWidget);
   });
